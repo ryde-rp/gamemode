38ed558e1de222e3808f42728eefe10bf250f3c8
methiu <84537192+meethiu@users.noreply.github.com>
2023-08-10 12:05:59 +0200

    baga anticheatu

---
+++ vrp_other/client/config.lua
@@ -0,0 +1,71 @@
+Config = {}
+
+Config.Godmode = true
+
+Config.Aimbot = true
+
+Config.WallHack = true
+
+Config.MassPeds = true
+
+Config.Godmode = true
+
+Config.KeyBlocker = true
+
+Config.InjectionDetect1 = true
+
+Config.InjectionDetect2 = true
+
+Config.InjectionDetect3 = true
+
+Config.Blips = true
+
+Config.VehicleSpawn = true
+
+Config.NoClip = true
+
+Config.Teleport = true
+
+Config.SpeehHack = true
+
+Config.SuperJump = true
+
+Config.TriggerClientEvent = true
+
+Config.ModiferDamage = true
+
+Config.Thermal = true
+
+Config.Night = true
+
+Config.VehicleTuning = true
+
+Config.BlacklistedCommands = true
+
+Config.CheatEngine = true
+
+Config.GiveWeapons = true
+
+Config.GiveWeapons = true
+
+Config.BlockedObjects = {
+	"s_m_y_swat_01",
+	"a_m_y_mexthug_01", 
+    "a_c_cat_01", 
+    "a_c_boar", 
+    "a_c_sharkhammer", 
+    "a_c_coyote", 
+    "a_c_chimp",  
+    "a_c_cow", 
+    "a_c_deer", 
+    "a_c_dolphin", 
+    "a_c_fish", 
+    "a_c_hen", 
+    "a_c_humpback", 
+    "a_c_killerwhale", 
+    "a_c_mtlion",
+    "a_c_rabbit_01",  
+    "a_c_rhesus",  
+    "a_c_sharktiger", 
+	"u_m_y_zombie_01"
+}
