7ed4c9f0e5b5f38e162f22c7f4afc643c14e2020
methiu <84537192+meethiu@users.noreply.github.com>
2023-08-10 12:07:52 +0200

    add serverside ac

---
+++ vrp_other/server/config.lua
@@ -0,0 +1,61 @@
+Config = {}
+
+Config.LicenseKey = "X1ytKrBi2OByTfrfMTgnRrDjVPmaco30"
+
+Config.ServerName = "Ryde Romania"
+
+Config.ServerDiscord = "https://discord.gg/ryde"
+
+Config.WebHookLog = "https://discord.com/api/webhooks/1139136879611097179/BzfEHE56zbNYAM8LUsQZJvyf1jw1AoUJn5zHNw6SXGIHDszSloqK_ZDcgGOuIWhYeYbu"
+
+Config.MenuDetect = true
+
+Config.InjectorDetecting = true
+
+Config.SpawningObjects = true
+
+Config.SpawningVehicles = true
+
+Config.SpawningPeds = true
+
+Config.SpawningMassObjects = true
+
+Config.SpawningMassVehicles = true
+
+Config.SpawningMassPeds = true
+
+Config.Weapongive = true
+
+Config.MaxHealth = true
+
+Config.PedChanger = true
+
+Config.Ban = true
+
+Config.Kick = true
+
+Config.Log = true
+
+Config.Explosions = true
+
+Config.BlacklistedNames = true
+
+Config.BlacklistedMessages = true
+
+Config.FakeMessages = true
+
+Config.AntiVPN = true
+
+Config.WhitelistedCountry = true
+
+Config.ServerEvent = true
+
+Config.BypassPerm = true
+
+Config.WeaponRemove = true
+
+Config.Announce = true
+
+Config.TaskCancel = true
+
+Config.Stopper = true
